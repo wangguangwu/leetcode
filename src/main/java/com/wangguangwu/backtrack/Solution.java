579cf0d5ef4a1c09faef69e33d835f18f481dec4
wangguangwu <wangguangwu@pukanghealth.com>
2023-08-02 10:10:44 +0800

    46. 全排列

---
+++ src/main/java/com/wangguangwu/backtrack/Solution.java
@@ -0,0 +1,56 @@
+package com.wangguangwu.backtrack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author wangguangwu
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.permute(new int[]{1, 2, 3});
+        for (List<Integer> integers : result) {
+            System.out.println(integers);
+        }
+    }
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    /**
+     * 主函数，输入一组不重复的数字，返回他们的全排列
+     */
+    List<List<Integer>> permute(int[] nums) {
+        // 记录路径
+        LinkedList<Integer> track = new LinkedList<>();
+        // 「路径」中的元素会被标记为 true，避免重复使用
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, track, used);
+        return res;
+    }
+
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 排除已经使用过的选择
+            if (used[i]) {
+                // nums[i] 已经在 track 中，跳过
+                continue;
+            }
+            // 做选择
+            track.add(nums[i]);
+            used[i] = true;
+            // 进入下一层决策树
+            backtrack(nums, track, used);
+            // 取消选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
