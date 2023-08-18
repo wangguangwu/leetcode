76d8b112b80aaf1cdaa3628bc8ba35ce3898eaef
wangguangwu <wangguangwu@pukanghealth.com>
2023-08-18 17:46:50 +0800

    567. 字符串的排列

--- src/main/java/com/wangguangwu/doublepointer/CheckInclusion.java
+++ src/main/java/com/wangguangwu/doublepointer/CheckInclusion.java
@@ -9,48 +9,47 @@ public class CheckInclusion {
 
     public static void main(String[] args) {
         CheckInclusion solution = new CheckInclusion();
-        boolean b = solution.checkInclusion("trinitrophenylmethylnitramine", "dinitrophenylhydrazinetrinitrophenylmethylnitramine");
+        boolean b = solution.checkInclusion("abc", "bbbca");
         System.out.println(b);
     }
 
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
         // 需要的数据
-        for (int i = 0; i < s1.length(); i++) {
-            char c = s1.charAt(i);
+        HashMap<Character, Integer> need = new HashMap<>();
+        // 窗口
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
             need.put(c, need.getOrDefault(c, 0) + 1);
         }
 
-        // 双指针维护窗口
+        // 双指针
         int left = 0;
         int right = 0;
         int valid = 0;
+
         while (right < s2.length()) {
             char c = s2.charAt(right);
-            right++;
-            if (need.containsKey(c)) {
-                window.put(c, window.getOrDefault(c, 0) + 1);
-                if (window.get(c).equals(need.get(c))) {
-                    valid++;
-                }
-            }
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            // 判断左侧窗口是否收缩
-            while (right - left >= s1.length()) {
-                // 判断是否找到了合法的子串
-                if (valid == need.size()) {
+            if (need.containsKey(c) && window.get(c).equals(need.get(c))) {
+                valid++;
+            }
+            right++;
+
+            // 满足条件
+            while (valid == need.size()) {
+                if (right - left == s1.length()) {
                     return true;
                 }
-                // 要移出的数据
+
                 char d = s2.charAt(left);
-                left++;
-                if (need.containsKey(d)) {
-                    if (window.get(d).equals(need.get(d))) {
-                        valid--;
-                    }
-                    window.put(d, window.getOrDefault(d, 0) - 1);
+
+                if (need.containsKey(d) && window.get(d).equals(need.get(d))) {
+                    valid--;
                 }
+                window.put(d, window.get(d) - 1);
+                left++;
             }
         }
         return false;
