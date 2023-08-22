c136dba274b1df0ac3b4ffcdb71e342298c3cc33
wangguangwu <wangguangwu@pukanghealth.com>
2023-08-22 11:02:00 +0800

    309. 买卖股票的最佳时机含冷冻期

---
+++ src/main/java/com/wangguangwu/stock/MaxProfit3.java
@@ -0,0 +1,35 @@
+package com.wangguangwu.stock;
+
+/**
+ * @author wangguangwu
+ */
+public class MaxProfit3 {
+
+    public static void main(String[] args) {
+        MaxProfit3 solution = new MaxProfit3();
+        int result = solution.maxProfit(new int[]{1, 2, 3, 0, 2});
+        System.out.println(result);
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                // base case
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            if (i - 2 == -1) {
+                // base case
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
